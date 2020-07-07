eebf9fdc03dd26471caae5c62b7c42b9091e4694
Adhitya Kusumadinatha <adhityasantika@yahoo.com>
2020-07-07 11:57:55 +0800

    Done : Logic Welcome Screen Add : Register and Login Activity

---
+++ app/src/main/java/com/gelora/pengguna/activity/Register.java
@@ -0,0 +1,16 @@
+package com.gelora.pengguna.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gelora.pengguna.R;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
--- app/src/main/java/com/gelora/pengguna/activity/WelcomeScreen.java
+++ app/src/main/java/com/gelora/pengguna/activity/WelcomeScreen.java
@@ -2,15 +2,59 @@ package com.gelora.pengguna.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.gelora.pengguna.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class WelcomeScreen extends AppCompatActivity {
+    FirebaseAuth auth;
+    Button daftar, masuk;
+    private long backPressedTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+        auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+        daftar = findViewById(R.id.daftarButton);
+        masuk = findViewById(R.id.masukButton);
+
+        daftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Register.class));
+            }
+        });
+
+        masuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+            }
+        });
+    }
+    @Override
+    public void onBackPressed() {
+
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            super.onBackPressed();
+            finishAffinity();
+            System.exit(0);
+            return;
+        } else {
+            Toast.makeText(getBaseContext(), "Tekan kembali lagi untuk keluar", Toast.LENGTH_SHORT).show();
+        }
+
+        backPressedTime = System.currentTimeMillis();
+
     }
 }
