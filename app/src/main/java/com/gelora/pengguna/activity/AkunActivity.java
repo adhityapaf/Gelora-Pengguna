f0e6ccbe3b9bb887b774883f57a3806a0f645ed9
Adhitya Kusumadinatha <adhityasantika@yahoo.com>
2020-10-19 22:01:41 +0800

    fix android 6.0 marshmallow can't run the app. reduced file size. updated the gradle.

--- app/src/main/java/com/gelora/pengguna/activity/AkunActivity.java
+++ app/src/main/java/com/gelora/pengguna/activity/AkunActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,7 +14,12 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.gelora.pengguna.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.OAuthCredential;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -27,6 +33,7 @@ public class AkunActivity extends AppCompatActivity {
     DatabaseReference userRef;
     String nama, email, passwrd;
     ImageView backButton;
+    AuthCredential credential;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +52,8 @@ public class AkunActivity extends AppCompatActivity {
         simpanPerubahan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                simpanPerubahan.setBackground(getDrawable(R.drawable.disabled_green_stroke_rectangle));
+                simpanPerubahan.setText("Menyimpan Perubahan....");
                 nama = namaMitra.getText().toString();
                 passwrd = passwordMitra.getText().toString();
                 if (TextUtils.isEmpty(nama)) {
@@ -55,10 +64,25 @@ public class AkunActivity extends AppCompatActivity {
                     passwordMitra.setError("Password tidak boleh kosong.");
                     return;
                 }
-                userRef.child("nama").setValue(nama);
-                userRef.child("password").setValue(passwrd);
-                FirebaseAuth.getInstance().getCurrentUser().updatePassword(passwrd);
-                Toast.makeText(AkunActivity.this, "Berhasil simpan perubahan akun", Toast.LENGTH_LONG).show();
+                FirebaseAuth.getInstance().getCurrentUser().reauthenticate(credential);
+                FirebaseAuth.getInstance().getCurrentUser().updatePassword(passwrd).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        userRef.child("nama").setValue(nama);
+                        userRef.child("password").setValue(passwrd);
+                        simpanPerubahan.setBackground(getDrawable(R.drawable.button_green_rounded_5dp));
+                        simpanPerubahan.setText("Simpan Perubahan");
+                        Toast.makeText(AkunActivity.this, "Berhasil simpan perubahan akun", Toast.LENGTH_LONG).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        simpanPerubahan.setBackground(getDrawable(R.drawable.button_green_rounded_5dp));
+                        simpanPerubahan.setText("Simpan Perubahan");
+                        Toast.makeText(AkunActivity.this, "Gagal Mengubah Profil, Coba lagi.", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                });
             }
         });
         logout.setOnClickListener(new View.OnClickListener() {
@@ -120,6 +144,8 @@ public class AkunActivity extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 passwrd = snapshot.getValue().toString();
                 passwordMitra.setText(passwrd);
+                credential = EmailAuthProvider.getCredential(email,passwrd);
+                Log.d(TAG, "AuthCred: "+credential);
             }
 
             @Override
